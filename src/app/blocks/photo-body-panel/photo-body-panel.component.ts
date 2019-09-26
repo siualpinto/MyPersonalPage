4d9af5c5f4d236866b5a68c6591720a639d1695b
Berto <ei12108@fe.up.pt>
2019-09-26 20:51:18 +0100

    improve photo page and profile

--- src/app/blocks/photo-body-panel/photo-body-panel.component.ts
+++ src/app/blocks/photo-body-panel/photo-body-panel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-body-panel.component.scss']
 })
 export class PhotoBodyPanelComponent implements OnInit {
-
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   constructor() { }
 
   ngOnInit() {
--- src/app/blocks/profile-body-panel/profile-body-panel.component.ts
+++ src/app/blocks/profile-body-panel/profile-body-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLinkedin, faStackOverflow, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-profile-body-panel',
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileBodyPanelComponent implements OnInit {
   stackOverflowLink = 'https://stackoverflow.com/users/9840062/luis-pinto?tab=profile';
   linkedin = 'https://www.linkedin.com/in/luis-a-m-p/';
-
+  github = 'https://github.com/siualpinto';
+  faLinkedin = faLinkedin;
+  faStackOverflow = faStackOverflow;
+  faGithub = faGithub;
   constructor() { }
 
   ngOnInit() {
